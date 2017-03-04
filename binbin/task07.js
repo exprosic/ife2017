c78e1e1216cae2d6c328f762642b301536ad95b5
exprosic.windows <exprosic@gmail.com>
2017-03-05 00:15:08 +0100

    finish task07

---
+++ binbin/task07.js
@@ -0,0 +1,45 @@
+window.onload = () => {
+	const root = [[[[],[]],[[],[]]],[[[],[]],[[],[]]]];
+
+	const renderTree = (x) => {
+		const node = document.createElement('div');
+		node.className = 'node';
+		for (const child of x)
+			node.appendChild(renderTree(child));
+		return node;
+	};
+
+	const setMark = (node) => {node.className += ' visiting';};
+	const clearMark = (node) => {node.className = 'node';};
+
+	const doTraverse = (order) => async function() {
+		for (const b of buttons)
+			b[1].setAttribute('disabled', 'disabled');
+
+		traverse = async function(x) {
+			for (const t of order) {
+				if (t == null) {
+					setMark(x);
+					await (new Promise(r => setTimeout(r, 500)));
+					clearMark(x);
+				} else if (t < x.children.length) {
+					await traverse(x.children[t]);
+				}
+			}
+		};
+
+		await traverse(container.children[0]);
+		for (const b of buttons)
+			b[1].removeAttribute('disabled');
+	};
+
+	const preOrder  = doTraverse([null, 0, 1]);
+	const inOrder   = doTraverse([0, null, 1]);
+	const postOrder = doTraverse([0, 1, null]);
+
+	const container = document.getElementById('container');
+	container.appendChild(renderTree(root));
+	const buttons = new Map(['pre-order', 'in-order', 'post-order'].map((b) => [b,document.getElementById(b)]));
+	[['pre-order',preOrder], ['in-order',inOrder], ['post-order',postOrder]].forEach(
+		([n,f]) => buttons.get(n).addEventListener('click', f));
+};
